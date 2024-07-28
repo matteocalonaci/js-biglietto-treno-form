41505f27f31db3a3dc5b28116094fc5f11ae9f6d
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-28 18:57:30 +0200

    Add commenti per rendere più comprensibile il codice

--- JS/main.js
+++ JS/main.js
@@ -5,24 +5,34 @@
 // prezzo base 0.21 euro/km
 
 let eta, distanza, sconto, prezzoBiglietto
+// Prezzo al km
 const prezzoBase = 0.21
-// Capire eta del passeggero
+
+// Recupero l'età del passeggero tramite la scelta nella select
 const etaElement = document.getElementById('eta')
-// Capire la distanza da percorrere
+// Recupero i km percorsi dall'input compilato dall'utente
 const distanzaElement = document.getElementById('distanza')
+// Invio i dati del form tramite il button
 const submitElement = document.getElementById('submit')
+
+
+
+// Recupero  il prezzo intero del biglietto
 const prezzoBaseElement = document.getElementById('prezzoBase')
+// Recupero lo sconto da applicare
 const scontoElement = document.getElementById('sconto')
+// Recupero il prezzo finale del biglietto
 const totaleElement = document.getElementById('totale')
-const warningElement = document.getElementById('warning')
+
 // Calcolare prezzo biglieto a base di chilometri
-
-
-
 submitElement.addEventListener('click', function () {
+    // Recupero l'età del passeggero tramite la scelta nella select
     eta = etaElement.value
+    // Recupero i km percorsi dall'input compilato dall'utente
     distanza = distanzaElement.value
+    // Creo una costante con il prezzo base moltiplicato in base ai km inseriti nell'input
     prezzoBiglietto = (prezzoBase * distanza)
+    // In base alle scelte dell'utente verrà stampato l'opzione corretta
     if (eta === 'minorenne') {
         // calcolo sconto del 20% moltiplico prezzo biglietto per 0.2
         sconto = (prezzoBiglietto * 0.2)
